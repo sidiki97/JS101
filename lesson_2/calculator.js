ddbb8857436d6f797793bc7c48547922a4535d11
sidiki97 <62025524+sidiki97@users.noreply.github.com>
2023-01-26 00:42:02 +0000

    update validation on calculator.js

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -8,26 +8,50 @@ const readline = require('readline-sync');
 
 console.log('Welcome to Calculator!');
 
-console.log("What's the first number?");
+function prompt (message) {
+  console.log(`=> ${message}`);
+}
+
+
+prompt("What's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt("What's the second number?");
 let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = readline.question();
+}
 
 console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
-switch(operation){
-  case "1": 
-    console.log(Number.parseInt(number1) + Number.parseInt(number2));
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
+
+switch (operation) {
+  case "1":
+    console.log(Number(number1) + Number(number2));
     break;
-  case "2": 
-    console.log(Number.parseInt(number1) - Number.parseInt(number2));
+  case "2":
+    console.log(Number(number1) - Number(number2));
     break;
-  case "3": 
-    console.log(Number.parseInt(number1) * Number.parseInt(number2));
+  case "3":
+    console.log(Number(number1) * Number(number2));
     break;
-  case "4": 
-    console.log(Number.parseInt(number1) / Number.parseInt(number2));
+  case "4":
+    console.log(Number(number1) / Number(number2));
     break;
 }
