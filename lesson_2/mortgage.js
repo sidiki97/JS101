d31544054ea7d7edb7f31535b0b7180cefe916ff
sidiki97 <62025524+sidiki97@users.noreply.github.com>
2023-02-20 20:20:46 +0000

    finished mortgage

--- lesson_2/mortgage.js
+++ lesson_2/mortgage.js
@@ -10,28 +10,10 @@ let prompt = (message) => {
   console.log(`=> ${message}`);
 };
 
-prompt("Enter loan amount: ");
-let loanAmount = readline.question();
-
-while (Number.isNaN(Number(loanAmount))) {
-  prompt("Enter loan amount: ");
-  loanAmount = readline.question();
-}
-
-prompt("Enter APR (%): ");
-let apr = readline.question();
-
-while (Number.isNaN(Number(apr))) {
-  prompt("Enter APR (%): ");
-  apr = readline.question();
-}
-
-prompt("Enter loan duration (years): ");
-let loanDuration = readline.question();
-
-while (Number.isNaN(Number(loanDuration))) {
-  prompt("Enter loan duration (years): ");
-  loanDuration = readline.question();
+function isInvalidNumber(number) {
+  return number.length === 0 ||
+         Number(number) < 0   ||
+         Number.isNaN(Number(number));
 }
 
 function durationInMonths (duration) {
@@ -49,5 +31,46 @@ function calculateMonthlyPayment (loanAmount, monthlyRate, duration) {
   return monthlyPayment;
 }
 
-console.log(`The monthly payment for a loan amount of $${loanAmount} with ${loanDuration} years duration 
-is $${calculateMonthlyPayment(loanAmount, monthlyInterestRate(apr), durationInMonths(loanDuration)).toFixed(2)}`);
+prompt("Welcome to Mortgage Calculator");
+
+let current = {'y' : true, 'n' : false};
+let pick = 'y';
+
+while (current[pick]) {
+
+  prompt("Enter loan amount: ");
+  let loanAmount = readline.question();
+
+  while (isInvalidNumber(Number(loanAmount))) {
+    prompt("Enter loan amount: ");
+    loanAmount = readline.question();
+  }
+
+  prompt("Enter APR (%): ");
+  let apr = readline.question();
+
+  while (isInvalidNumber(Number(apr))) {
+    prompt("Enter APR (%): ");
+    apr = readline.question();
+  }
+
+  prompt("Enter loan duration (years): ");
+  let loanDuration = readline.question();
+
+  while (isInvalidNumber(Number(loanDuration))) {
+    prompt("Enter loan duration (years): ");
+    loanDuration = readline.question();
+  }
+
+  console.log(`The monthly payment for a loan amount of $${loanAmount} with ${loanDuration} years duration 
+  is $${calculateMonthlyPayment(loanAmount, monthlyInterestRate(apr), durationInMonths(loanDuration)).toFixed(2)}`);
+
+  prompt("Another calculation?");
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+
+  pick = answer[0];
+}
